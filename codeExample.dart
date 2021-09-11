73aefed227c63301eda5217f71ce4f16eb0e10e1
EL ADOUI Mohammed <mohammed.eladoui.phd@gmail.com>
2021-09-11 12:12:44 +0200

    change dart

--- codeExample.dart
+++ codeExample.dart
@@ -17,7 +17,7 @@ void main() {
           20,
           (i) => Todo(
             'Todo $i',
-            'A description of what needs to be done for Todo $i',
+            'A description YES of what needs to be done for Todo $i',
           ),
         ),
       ),
